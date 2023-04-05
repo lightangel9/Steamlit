56f463bc53227f730b97f57b3dec6e4de2daaad9
lightangel9 <128405727+lightangel9@users.noreply.github.com>
2023-04-05 22:30:59 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 import streamlit as st
+import Dataset
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
-import Dataset
+
 
 menu = ["หน้าหลัก", "หน้าที่สอง"]
 choice = st.sidebar.selectbox("ไปที่", menu)
