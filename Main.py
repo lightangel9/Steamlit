4ef990aea154959c27b0d2b59ca3a8503e7a53a4
lightangel9 <128405727+lightangel9@users.noreply.github.com>
2023-04-07 01:11:48 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from predictions
-from dataset
-from ref_code
+from pages.page_1_predictions import show as page_1_predictions_show
+from pages.page_2_dataset import show as page_2_dataset_show
+from pages.page_3_ref_code import show as page_3_ref_code_show
 
 # เมนูเปลี่ยนหน้า
 menu = ['1_Predictions', '2_Dataset', '3_RefCode']
@@ -9,9 +9,8 @@ choice = st.sidebar.selectbox("เลือกหน้าเว็บ", menu)
 
 # ตรวจสอบเลือกหน้าเว็บและแสดงเนื้อหาตามที่เลือก
 if choice == '1_Predictions':
-    1_predictions()
+    page_1_predictions_show()
 elif choice == '2_Dataset':
-    2_dataset()
+    page_2_dataset_show()
 elif choice == '3_RefCode':
-    3_ref_code)
-
+    page_3_ref_code_show()
