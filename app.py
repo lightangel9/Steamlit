0e57d9adf88c0735a9be9e623d8e2d8e18bab1d4
lightangel9 <128405727+lightangel9@users.noreply.github.com>
2023-04-07 10:46:35 +0700

    Update and rename Main.py to app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from multiapp import MultiApp
+from Pages import Predictions, Dataset, RefCode
+
+app = MultiApp()
+
+# Add all your application here
+app.add_app("Predictions", Predictions.app)
+app.add_app("Dataset", Dataset.app)
+app.add_app("RefCode", RefCode.app)
+# The main app
+app.run()
