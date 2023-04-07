34c1c79b520896085b2862cf960aeef4e4f9c299
lightangel9 <128405727+lightangel9@users.noreply.github.com>
2023-04-07 12:02:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import Predictions, Dataset, RefCode
+from apps import Dataset, Predictions, RefCode
 
 app = MultiApp()
 
 # Add all your application here
-app.add_app("Predictions", Predictions.app)
 app.add_app("Dataset", Dataset.app)
+app.add_app("Predictions", Predictions.app)
 app.add_app("RefCode", RefCode.app)
 app.run()
