463203c17cf49b78a37ba569ac963cec1590143d
lightangel9 <128405727+lightangel9@users.noreply.github.com>
2023-04-07 14:56:22 +0700

    Update Predictions.py

--- Predictions.py
+++ Predictions.py
@@ -170,7 +170,7 @@ if uploaded_file is not None:
             
            
 st.sidebar.markdown("""
-    #About 
+    # About 
     This project involves processing images of all 120 dog breeds from around the world using deep learning models. 
     """)
 st.sidebar.warning("**💾 More informations:**")
